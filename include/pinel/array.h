9cd5e9d7007479bef9fe6474f6267351fa0a6324
moris071651 <moris071651@proton.me>
2024-11-17 14:06:01 +0200

    Added create function

--- include/pinel/array.h
+++ include/pinel/array.h
@@ -8,5 +8,18 @@
 
 typedef struct pinel_array_t pinel_array_t;
 
+/**
+ * @brief Creates a new dynamic array.
+ *
+ * This function initializes and returns a new dynamic array that can store elements
+ * of a given size. The array will be initialized with zero length and an initial
+ * capacity, and you can specify the deallocation and comparison functions.
+ * 
+ * @param elem_size The size (in bytes) of each element that the array will hold.
+ * @param dealloc A function to deallocate elements when the array is modified (optional).
+ * @param cmp A comparison function for sorting and comparisons (optional).
+ * @return A pointer to the created dynamic array.
+ */
+pinel_array_t* pinel_array_create(size_t elem_size, pinel_dealloc_t dealloc, pinel_cmp_t cmp);
 
 #endif // PINEL_ARRAY_H
--- src/array.c
+++ src/array.c
@@ -11,3 +11,21 @@ struct pinel_array_t {
     pinel_cmp_t cmp;
     pinel_dealloc_t dealloc;
 };
+
+pinel_array_t* pinel_array_create(size_t elem_size, pinel_dealloc_t dealloc, pinel_cmp_t cmp) {
+    pinel_array_t* self = malloc(sizeof(pinel_array_t));
+    if (!self) {
+        return NULL;
+    }
+
+    self->array = NULL;
+
+    self->length = 0;
+    self->capacity = 0;
+
+    self->cmp = cmp;
+    self->dealloc = dealloc;
+    self->elem_size = elem_size;
+
+    return self;
+}
